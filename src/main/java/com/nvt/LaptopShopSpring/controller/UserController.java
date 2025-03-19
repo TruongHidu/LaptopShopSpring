caef8467d44a6b89f43fdea42c94e1fdadab6c77
TruongHidu <nguyenvantruong1009204@gmail.com>
2025-03-19 13:40:26 +0700

    19/3

--- src/main/java/com/nvt/LaptopShopSpring/controller/UserController.java
+++ src/main/java/com/nvt/LaptopShopSpring/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.nvt.LaptopShopSpring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.nvt.LaptopShopSpring.service.UserService;
@@ -13,9 +15,16 @@ public class UserController {
         this.userService = userService;
     }
 
-    @RequestMapping("/")
-    public String getHomePage() {
+    @GetMapping("/")
+    public String getHomePage(Model model) {
+        model.addAttribute("lol", "Comunity");
         return "hello";
+    }
+
+    @GetMapping("/user/create")
+    public String addUser(Model model) {
+        model.addAttribute("lol", "Comunity");
+        return "admin/user/create";
     }
 
 }
